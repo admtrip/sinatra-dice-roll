e1923c546e3e77c49a42ef6251538294574ba45a
admtrip <atripat0@chicagobooth.edu>
2025-01-25 23:18:02 +0000

    dice changes

--- dice.rb
+++ dice.rb
@@ -22,6 +22,26 @@ get ("/dice/2/6") do
 
   outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 
-  "<h1>2d6</h1>
+  get ("/dice/2/10") do 
+    first_die = rand(1.10)
+    second_die = rand(1..10)
+    sum = first_die + second_die
+  
+    outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+
+    get ("/dice/2/20") do 
+      first_die = rand(1.20)
+      second_die = rand(1..20)
+      sum = first_die + second_die
+    
+      outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+
+     get ("/dice/5/4") do 
+      first_die = rand(1.4)
+      second_die = rand(1..4)
+      sum = first_die + second_die
+    
+      outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  "<h1>Dice Roll</h1>
     <p>#{outcome}</p>"
 end
