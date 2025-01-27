675d7ece76347627d85549a0db0c424209726a89
admtrip <atripat0@chicagobooth.edu>
2025-01-27 04:58:23 +0000

    changed code

--- dice.rb
+++ dice.rb
@@ -2,46 +2,56 @@
 
 require "sinatra"
 require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
 
 get "/" do
-  "Hello World"
+  <<-HTML
+    <h1>Welcome to the Dice Roller</h1>
+    <p>Click a link below to roll dice:</p>
+    <ul>
+      <li><a href="/dice/2/6">Roll 2 six-sided dice</a></li>
+      <li><a href="/dice/2/10">Roll 2 ten-sided dice</a></li>
+      <li><a href="/dice/1/20">Roll 1 twenty-sided die</a></li>
+      <li><a href="/dice/5/4">Roll 5 four-sided dice</a></li>
+    </ul>
+  HTML
 end
 
-get("/zebra") do
-  "We must add a route to end each path we want to support"
-end 
-
-get ("/giraffe")do
-  "Hopefully this shows up without having to restart the server"
-end
+# Code for rolling 2 six-sided dice
 
 get ("/dice/2/6") do 
-  first_die = rand(1.6)
+  first_die = rand(1..6)
   second_die = rand(1..6)
   sum = first_die + second_die
 
   outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+end
 
-  get ("/dice/2/10") do 
-    first_die = rand(1.10)
-    second_die = rand(1..10)
-    sum = first_die + second_die
+# Code for rolling 2 ten-sided dice
+
+get ("/dice/2/10") do 
+  first_die = rand(1..10)
+  second_die = rand(1..10)
+  sum = first_die + second_die
   
-    outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+end
 
-    get ("/dice/2/20") do 
-      first_die = rand(1.20)
-      second_die = rand(1..20)
-      sum = first_die + second_die
-    
-      outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+# Code for rolling 1 twenty-sided dice
 
-     get ("/dice/5/4") do 
-      first_die = rand(1.4)
-      second_die = rand(1..4)
-      sum = first_die + second_die
-    
-      outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-  "<h1>Dice Roll</h1>
-    <p>#{outcome}</p>"
+get ("/dice/1/20") do 
+  first_die = rand(1..20)
+  sum = first_die + second_die
+  
+  outcome = "You rolled a #{first_die}."
+end
+
+# Code for rolling 5 four-sided dice
+
+get ("/dice/5/4") do 
+  dice = array.new(5) { rand(1..4) }
+  sum = dice.sum
+  
+  outcome = "You rolled a #{dice.join(', ')} for a total of #{sum}."
 end
